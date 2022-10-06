3555570518b97515d18dfa98c4fddc4c52be8fc5
ajautz@gmail.com <ajautz@gmail.com>
2022-10-06 10:22:35 -0400

    Add mutations to the graphql products

--- products/products.model.js
+++ products/products.model.js
@@ -2,12 +2,15 @@ const products = [
   {
     id: "redshoe",
     description: "Red Shoe",
-    price: 42.12
+    price: 42.12,
+    reviews: []
+
   },
   {
     id: "bluejean",
     description: "Blue Jeans",
-    price: 55.55
+    price: 55.55,
+    reviews: []
   }
 ];
 
@@ -27,6 +30,36 @@ function getProductById(id) {
   })
 }
 
+function addNewProduct(id, description, price)  {
+  const newProduct = {
+    id,
+    price,
+    description,
+    reviews: []
+  };
+
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+  const matchedProduct = getProductById(id);
+  if (matchedProduct) {
+    const newProductReview = {
+      rating,
+      comment,
+    };
+
+    matchedProduct.reviews.push(newProductReview);
+
+    return newProductReview;
+  }
+}
+
 module.exports = {
-  getAllProducts, getProductsByPrice, getProductById 
+  getAllProducts, 
+  getProductsByPrice, 
+  getProductById, 
+  addNewProduct,
+  addNewProductReview 
 }
